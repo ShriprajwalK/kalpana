5937366bc2c8105b7ce592836c4c173bd9a4d3d4
mundra-pranay <pranay.mundra@outlook.com>
2019-10-02 03:47:44 +0530

    4 hour sleep

--- toll_gate/user/models.py
+++ toll_gate/user/models.py
@@ -13,3 +13,13 @@ from django.contrib.auth.models import User
 #
 #     def __str__(self):
 #         return self.name+self.vehicle_number
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=CASCADE)
+    name = models.CharField(max_length=30)
+    birthdate = models.DateField(null=True, blank=True)
+    vehicle_number = models.CharField(max_length=11)
+    sex = models.CharField(max_length=1)
+    money = models.FloatField(default=0)
+    def __str__(self):
+        return self.user.useran
--- toll_gate/user/views.py
+++ toll_gate/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -14,3 +15,8 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'home/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
